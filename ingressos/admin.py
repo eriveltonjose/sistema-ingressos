43d8084c1943f60a1d7903d53306d88361c2b3ad
eriveltonjose <148704676+eriveltonjose@users.noreply.github.com>
2026-06-10 15:44:45 -0300

    Adiciona reenvio de ingressos pelo admin

--- ingressos/admin.py
+++ ingressos/admin.py
@@ -1,12 +1,44 @@
 from django.contrib import admin
 from .models import Evento, Ingresso, Pedido, ValidacaoAssociado
+from .views import enviar_email_ingressos
 
 admin.site.register(ValidacaoAssociado)
 
 admin.site.register(Evento)
 
+def reenviar_ingresso(modeladmin, request, queryset):
+
+    enviados = 0
+
+    for ingresso in queryset:
+
+        try:
+            enviar_email_ingressos(
+                [ingresso],
+                ingresso.email
+            )
+
+            enviados += 1
+
+        except Exception as erro:
+
+            modeladmin.message_user(
+                request,
+                f'Erro ao enviar para {ingresso.email}: {erro}'
+            )
+
+    modeladmin.message_user(
+        request,
+        f'{enviados} ingresso(s) reenviado(s) com sucesso.'
+    )
+
+
+reenviar_ingresso.short_description = '📧 Reenviar ingresso por e-mail'
 @admin.register(Ingresso)
 class IngressoAdmin(admin.ModelAdmin):
+
+    actions = [reenviar_ingresso]
+    
     list_display = (
         'id',
         'nome_comprador',
